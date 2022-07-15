eb1ad9e39790f7cbf675bef828d3ecb9375dab8b
Lyamina Ekaterina <katjamina@rambler.ru>
2022-07-15 10:35:19 +0300

    Задача 15 решена

---
+++ Ex15/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int number=new Random().Next[1,7];
+Console.WriteLine($"number:{number}");
+ReadLine(n);
+Case n of;
+1=WriteLine ("Рабочий день");
+2=WriteLine ("Рабочий день");
+3=WriteLine ("Рабочий день");
+4=WriteLine ("Рабочий день");
+5=WriteLine ("Рабочий день");
+6=WriteLine ("Выходной день");
+7=WriteLine ("Выходной день");
